1e14a7e2690c5a7d8b9fdb34b7bb14400d2b26fd
Victor Almeida França <fr4ncacc@gmail.com>
2019-05-17 10:37:51 -0300

    nfc

---
+++ src/pages/Main/index.js
@@ -0,0 +1,67 @@
+import React, { PureComponent } from "react";
+import { startNFC, stopNFC } from "../../components/NFCHelper";
+
+import { Text, FlatList, View, StyleSheet } from "react-native";
+
+class Main extends PureComponent {
+  state = {
+    tag: false,
+    list: [
+      {
+        key: "Item"
+      }
+    ]
+  };
+
+  componentWillMount() {
+    startNFC(this.handleNFCTagReading);
+  }
+
+  componentWillUnmount() {
+    stopNFC();
+  }
+
+  handleNFCTagReading = nfcResult => {
+    let item = "Mais um item";
+    if (nfcResult) {
+      this.setState({
+        ...this.state,
+        tag: !this.state.tag
+      });
+      console.log(this.state.tag);
+    }
+    if (this.state.tag) {
+      this.setState({
+        ...this.state,
+        tag: !this.state.tag,
+        list: [...this.state.list, { key: "Mais um item" }]
+      });
+    }
+  };
+
+  render() {
+    const { list } = this.state;
+    return (
+      <View style={styles.container}>
+        <FlatList
+          data={list}
+          renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44
+  }
+});
+
+export default Main;
